8cd6ebaba784689e5dcfe26cd431071d2da689d7
Isaías Malvar Guíos <isaiasmg93@hotmail.com>
2024-02-19 19:17:29 +0000

    responsiveness added for main components

--- src/components/SuggestionList.tsx
+++ src/components/SuggestionList.tsx
@@ -7,7 +7,7 @@ const SuggestionList = (): React.ReactElement => {
   return (
     <>
       {((showSuggestions && suggestions.length > 1) || error) && (
-        <ul className="font-mono text-2xl  bg-white absolute border top-[70px] left-0 border-gray-300 rounded-md min-w-[200px]  flex flex-col gap-1 py-2 px-2">
+        <ul className="font-mono text-2xl  bg-white absolute border top-[70px] left-0 border-gray-300 rounded-md min-w-[200px]  flex flex-col gap-1 py-2 px-2 z-10">
           {error && suggestions.length < 1 && (
             <li className="text-red-500 p-1 "> {error}</li>
           )}
@@ -15,7 +15,7 @@ const SuggestionList = (): React.ReactElement => {
             <li
               key={i}
               onClick={() => handleSuggestionClick(item)}
-              className="cursor-pointer p-1 rounded   hover:bg-gray-200"
+              className="cursor-pointer p-1 rounded   hover:bg-gray-200 text-blue-500"
             >
               {item}
             </li>
